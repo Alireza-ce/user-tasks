54aa0de02ad196900b0ec335786cf41cdb3bbff3
Alireza <alireza.rezaei@tapsi.cab>
2024-09-09 11:30:27 +0330

    fix: multi theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
-import './App.css'
 import {ThemeProvider} from './contexts/ThemeContext.tsx';
 import ThemeToggle from './components/ThemeToggle';
 import {AntProvider} from './providers/AntProvider.tsx';
 import UserTasks from './components/UserTaks';
 import {TaskModalProvider} from './contexts/TaskModalContext.tsx';
+import styles from './App.module.scss'
+import AppLayout from './components/AppLayout';
 
 function App() {
   return (
-      <ThemeProvider>
-          <AntProvider>
-              <ThemeToggle />
-              <TaskModalProvider>
-                  <UserTasks/>
-              </TaskModalProvider>
-          </AntProvider>
-      </ThemeProvider>
+      <div className={styles.app}>
+          <ThemeProvider>
+              <AntProvider>
+                  <AppLayout>
+                      <ThemeToggle/>
+                      <TaskModalProvider>
+                          <UserTasks/>
+                      </TaskModalProvider>
+                  </AppLayout>
+              </AntProvider>
+          </ThemeProvider>
+      </div>
   )
 }
 
---
+++ src/components/AppLayout/AppLayout.tsx
@@ -0,0 +1,11 @@
+import {useContext} from 'react';
+import {ThemeContext} from '../../contexts/ThemeContext.tsx';
+import styles from './AppLayout.module.scss'
+
+const AppLayout = ({children}) => {
+    const {theme} = useContext(ThemeContext)
+
+    return (<div className={theme === 'dark' ? styles.darkMode : styles.lightMode}>{children}</div>)
+}
+
+export default AppLayout
